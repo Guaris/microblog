a53d1e87d46c9b10aa540631d235eb311ab613f0
Angel <aguarisma@linode.com>
2019-07-26 14:21:56 -0400

    Added login functionallity and error messages

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
-from flask import render_template
-import app
+from flask import render_template, flash, redirect, url_for
+from app import app
 from app.forms import LoginForm
 
 
-@app.app.route('/')
-@app.app.route('/index')
+@app.route('/')
+@app.route('/index')
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -20,7 +20,11 @@ def index():
 
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login Requested for user {}, remember_me{}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign in', form=form)
